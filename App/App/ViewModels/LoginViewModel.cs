628e6f5e621293e6017d506da32c88d91637113a
Sirley1409 <sirleycristinaflorez@gmail.com>
2023-07-22 20:44:18 -0500

    se sube el codigo de la app

---
+++ App/App/ViewModels/LoginViewModel.cs
@@ -0,0 +1,113 @@
+﻿using App.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        private string _username;
+        private string _password;
+        private bool _showMessage;
+        private string _welcomeMessage;
+        private Color _messageColor;
+
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                if (_username != value)
+                {
+                    _username = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (_password != value)
+                {
+                    _password = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public Color MessageColor
+        {
+            get => _messageColor;
+            set
+            {
+                if (_messageColor != value)
+                {
+                    _messageColor = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public bool ShowMessage
+        {
+            get => _showMessage;
+            set
+            {
+               if (_showMessage != value)
+                {
+                    _showMessage = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string WelcomeMessage
+        {
+            get => _welcomeMessage;
+            set
+            {
+                if (_welcomeMessage != value)
+                {
+                    _welcomeMessage = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public Command LoginCommand { get; }
+
+        public string UserName { get; set; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(OnLoginClicked);
+        }
+
+        private async void OnLoginClicked(object obj)
+        {
+            if (ValidateFiels())
+            {
+                await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+            }
+            else
+            {
+                ShowMessage = true;
+                MessageColor = Color.Red;
+                WelcomeMessage = "Usuario Invalido";
+            }
+
+        }
+
+        private bool ValidateFiels()
+        {
+            if (!string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ App/App/Views/LoginPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using App.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            //this.BindingContext = new LoginViewModel();
+        }
+        //private void OnLoginButtonClicked(object sender, EventArgs e)
+        //{
+        //    string username = UsernameEntry.Text;
+        //    string password = PasswordEntry.Text;
+
+        //    // Aquí puedes agregar la lógica de verificación del usuario y contraseña.
+        //    // Por ejemplo, puedes compararlos con datos almacenados en una base de datos o
+        //    // realizar una llamada a una API para autenticar al usuario.
+
+        //    // Ejemplo de validación simple (para propósitos de demostración):
+        //    if (username == "sflorez" && password == "1234")
+        //    {
+        //        // Si el usuario y la contraseña son válidos, puedes navegar a la siguiente página.
+        //        // Por ejemplo, si tienes una página llamada "MainPage.xaml", puedes hacer lo siguiente:
+        //        // await Navigation.PushAsync(new MainPage());
+
+        //        // Aquí, por fines de demostración, mostraremos un mensaje de éxito:
+        //        WelcomeLabel.Text = "Inicio de sesión exitoso, " + "¡Bienvenido! " + username + "!";
+        //        WelcomeLabel.IsVisible = true;
+        //        WelcomeLabel.TextColor = Color.Green;
+        //    }
+        //    else
+        //    {
+        //        // Si las credenciales son incorrectas, muestra un mensaje de error.
+        //        WelcomeLabel.Text = "Usuario o contraseña incorrectos";
+        //        WelcomeLabel.TextColor = Color.Red;
+        //        WelcomeLabel.IsVisible = true;
+        //        //DisplayAlert("Error de inicio de sesión", "Usuario o contraseña incorrectos", "OK");
+
+        //    }
+        //}
+    }
+}
